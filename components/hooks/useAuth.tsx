8e59d68ad4f525f3c12cbd14ffaedf8c75442ffb
Roho7 <rohosen2@gmail.com>
2024-06-22 10:30:33 +0530

    fix [auth rerouting]

--- components/hooks/useAuth.tsx
+++ components/hooks/useAuth.tsx
@@ -32,7 +32,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const signup = async (email: string, username: string, password: string) => {
     try {
-      console.log(email, username, password);
       const new_user = await createUserWithEmailAndPassword(
         auth,
         email,
@@ -56,9 +55,31 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       const res = await signInWithEmailAndPassword(auth, username, password);
       setUser(res.user);
       router.replace('/(protected)');
-      toast.show('Successfully signed in.', { native: false });
-    } catch (error) {
-      toast.show('Error signing in.', { native: false });
+    } catch (error: any) {
+      if (error.code) {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            alert('Invalid email address format.');
+            throw new Error('Invalid email address format.');
+          case 'auth/user-disabled':
+            alert('User account is disabled.');
+            throw new Error('User account is disabled.');
+          case 'auth/user-not-found':
+            alert('User not found. Please check your email and password.');
+            throw new Error(
+              'User not found. Please check your email and password.'
+            );
+          case 'auth/wrong-password':
+            alert('Incorrect password. Please try again.');
+            throw new Error('Incorrect password. Please try again.');
+          default:
+            alert('Failed to login User. Please try again later.');
+            throw new Error('Failed to login User. Please try again later.');
+        }
+      } else {
+        alert('An unexpected error occurred.');
+        throw new Error('An unexpected error occurred.');
+      }
     }
   };
 
