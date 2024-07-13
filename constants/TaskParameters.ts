c45edb2b5abb9bb8266e442eecd2e53c7b8ab0c5
Roho7 <rohosen2@gmail.com>
2024-07-13 09:30:35 +0100

    add [start date, end date to add task || organized types]

--- constants/TaskParameters.ts
+++ constants/TaskParameters.ts
@@ -1,6 +1,29 @@
-export const Priority = {
+import { PriorityMapType } from './types';
+
+export const PriorityMap: Record<number, PriorityMapType> = {
+  4: {
+    color: 'bg-red-500',
+    text: 'Urgent',
+    icon: 'alert-circle',
+  },
+  3: {
+    color: 'bg-orange-500',
+    text: 'High',
+    icon: 'chevrons-up',
+  },
+  2: {
+    color: 'bg-yellow-500',
+    text: 'Medium',
+    icon: 'chevron-up',
+  },
   1: {
-    value: 'Low',
-    color: '#4CAF50',
+    color: 'bg-green-500',
+    text: 'Low',
+    icon: 'chevron-down',
+  },
+  0: {
+    color: 'bg-gray-300',
+    text: 'No Priority',
+    icon: 'minus',
   },
 };
