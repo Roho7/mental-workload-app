d7087be12b95d9e560f09bfe64b78478ea80e861
Roho7 <rohosen2@gmail.com>
2024-07-15 22:13:32 +0100

    add [feedback for the day]

--- functions/src/ai.ts
+++ functions/src/ai.ts
@@ -1,4 +1,6 @@
 import { VertexAI } from '@google-cloud/vertexai';
+import * as functions from 'firebase-functions';
+
 import { logger } from 'firebase-functions/v1';
 import { MentalWorkloadRequestType } from '.';
 
@@ -14,16 +16,7 @@ export const vertex_ai = new VertexAI({
   location: 'asia-southeast1',
 });
 
-const model = 'gemini-1.5-flash';
-
-var systemInstruction = `You evaluate Mental Workload. You will be given details about 
-the user and their tasks in a day.
-Mental workload refers to the cognitive demands placed on an individual 
-while performing tasks or activities. Each level of MWL has positive aspects, 
-but maintaining any single level for extended periods can have negative consequences. 
-A cycle of different levels, is likely the best outcome when viewed across an entire day. 
-A score of 3 is considered optimal mental workload. 
-You can only return an object. Do not use backticks or format into markdown.`;
+const model = 'gemini-1.5-pro-preview-0409';
 
 // Instantiate the models
 export const ai = vertex_ai.preview.getGenerativeModel({
@@ -34,8 +27,29 @@ export const ai = vertex_ai.preview.getGenerativeModel({
     topP: 1,
     topK: 32,
   },
-  systemInstruction: systemInstruction,
+  systemInstruction: {
+    parts: [
+      { text: 'You evaluate Mental Workload.' },
+      {
+        text: ' You will be given details about the user and their tasks in a day. Mental workload refers to the cognitive demands placed on an individual while performing tasks or activities. Each level of MWL has positive aspects, but maintaining any single level for extended periods can have negative consequences. A cycle of different levels, is likely the best outcome when viewed across an entire day. A score of 3 is considered optimal mental workload.',
+      },
+      {
+        text: 'You must only return an object, do not use backticks, do not format into markdown.',
+      },
+    ],
+    role: 'system',
+  },
 });
+
+const removeJsonCodeBlockTags = (inputString: string) => {
+  const jsonCodeBlockPattern = /^```json\s+([\s\S]*?)\s+```$/;
+
+  const match = inputString.match(jsonCodeBlockPattern);
+  if (match) {
+    return match[1].trim();
+  }
+  return inputString;
+};
 
 export const getAIMwlRating = async ({
   raw_data,
@@ -45,20 +59,21 @@ export const getAIMwlRating = async ({
   isTemporaryFeedback: boolean;
 }): Promise<AIMwlReturnType | undefined | null> => {
   try {
+    functions.logger.info('Generating AI response for:', raw_data);
     const input = JSON.stringify(raw_data);
-
+    functions.logger.info('Input data:', input);
     // prompt for generating temprorary feedback on the day's planning
     const feedbackPrompt = `This object contains all the tasks that I have to do today: ${input}. But the day is not fully planned.
     Assign a mental workload score between 1 and 5 after assessing all the details and also give feedback on how the day is being structured under 60 words. 
     Return an object of type {userId: string, date: Timestamp (DD-MM-YYYY), 
-    mwl: number, feedback: string}. where mwl is the mental workload`;
+    mwl: number, feedback: string}. where mwl is the mental workload. Do not use backticks or format into markdown.`;
 
     // prompt for generating total mwl score
     const totalMwlPrompt = `This object contains all the tasks that I have to do today: ${input}. 
     Assign a mental workload score between 1 and 5 after assessing all the details available. 
     Return an object of type {userId: string, date: Timestamp (DD-MM-YYYY), 
     mwl: number, feedback: string}. where mwl is the mental 
-    workload score and the feedback is on the day's planning under 60 words.`;
+    workload score and the feedback is on the day's planning under 60 words. Do not use backticks or format into markdown.`;
 
     // construct the textPart
     const textPart = {
@@ -80,7 +95,9 @@ export const getAIMwlRating = async ({
       ?.replace(/(\w+):/g, '"$1":')
       .replace(/\'/g, '"');
 
-    let returnResponse: AIMwlReturnType = JSON.parse(correctedString!);
+    correctedString = removeJsonCodeBlockTags(correctedString || '');
+
+    let returnResponse: AIMwlReturnType = JSON.parse(correctedString);
 
     return returnResponse;
   } catch (error) {
