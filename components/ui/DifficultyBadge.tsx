226524e8bea20b81cc71956be794ec1cdf0e074f
Roho7 <rohosen2@gmail.com>
2024-07-15 16:56:32 +0100

    add [inbox]

--- components/ui/DifficultyBadge.tsx
+++ components/ui/DifficultyBadge.tsx
@@ -10,43 +10,6 @@ export type MwlType = {
   feedbackText: string;
 };
 
-export const MwlMap: Record<number, MwlType> = {
-  5: {
-    color: '#EA5757',
-    text: 'Overload',
-    icon: 'brain',
-    iconColor: '#DC2626',
-    feedbackText: 'Overload!',
-  },
-  4: {
-    color: '#F97316',
-    text: 'High Load',
-    icon: 'brain',
-    iconColor: '#FB7185',
-    feedbackText: 'One of those tough days',
-  },
-  3: {
-    color: '#34C51E',
-    text: 'Medium Load',
-    icon: 'brain',
-    iconColor: '#F59E0B',
-    feedbackText: 'Perfect balance',
-  },
-  2: {
-    color: '#A1DF3B',
-    text: 'Light Load',
-    icon: 'brain',
-    iconColor: '#34D399',
-    feedbackText: 'Breezier than usual',
-  },
-  1: {
-    color: '#82E9B2',
-    text: 'No Load',
-    icon: 'brain',
-    iconColor: '#BEF264',
-    feedbackText: 'Not enough challenge',
-  },
-};
 export const DifficultyMap: Record<number, any> = {
   5: {
     color: '#EA5757',
