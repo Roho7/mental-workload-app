5905f4e195ad55b032e53b330e529087c92c50b2
Roho7 <rohosen2@gmail.com>
2024-07-16 13:45:57 +0100

    fix  [fucntions and fetch logic for mwl]

--- functions/src/ai.ts
+++ functions/src/ai.ts
@@ -1,5 +1,4 @@
 import { VertexAI } from '@google-cloud/vertexai';
-import * as functions from 'firebase-functions';
 
 import { logger } from 'firebase-functions/v1';
 import { MentalWorkloadRequestType } from '.';
@@ -11,30 +10,32 @@ export type AIMwlReturnType = {
   feedback: string;
 };
 
-export const vertex_ai = new VertexAI({
+export const gemini = new VertexAI({
   project: 'mental-workload-app',
   location: 'asia-southeast1',
 });
 
-const model = 'gemini-1.5-pro-preview-0409';
+const model = 'gemini-1.5-flash';
 
 // Instantiate the models
-export const ai = vertex_ai.preview.getGenerativeModel({
+export const ai = gemini.preview.getGenerativeModel({
   model: model,
   generationConfig: {
     maxOutputTokens: 2048,
-    temperature: 0.4,
-    topP: 1,
-    topK: 32,
+    temperature: 0.5,
+    topP: 0.6,
+    topK: 35,
   },
   systemInstruction: {
     parts: [
-      { text: 'You evaluate Mental Workload.' },
       {
-        text: ' You will be given details about the user and their tasks in a day. Mental workload refers to the cognitive demands placed on an individual while performing tasks or activities. Each level of MWL has positive aspects, but maintaining any single level for extended periods can have negative consequences. A cycle of different levels, is likely the best outcome when viewed across an entire day. A score of 3 is considered optimal mental workload.',
+        text: 'You evaluate Mental Workload. You will be given details about me and my tasks today. You must only return an object, do not use backticks, do not format into markdown.',
       },
       {
-        text: 'You must only return an object, do not use backticks, do not format into markdown.',
+        text: 'Mental workload refers to the cognitive demands placed on an individual while performing tasks or activities. Each level of MWL has positive aspects, but maintaining any single level for extended periods can have negative consequences.',
+      },
+      {
+        text: 'A cycle of different levels, is likely the best outcome when viewed across an entire day. A score of 3 is considered optimal mental workload. Consider my preferences, day feedback and number of tasks to evaluate the mental workload.',
       },
     ],
     role: 'system',
@@ -53,23 +54,25 @@ const removeJsonCodeBlockTags = (inputString: string) => {
 
 export const getAIMwlRating = async ({
   raw_data,
-  isTemporaryFeedback = false,
+  isTemporary = false,
 }: {
   raw_data: MentalWorkloadRequestType;
-  isTemporaryFeedback: boolean;
+  isTemporary: boolean;
 }): Promise<AIMwlReturnType | undefined | null> => {
   try {
-    functions.logger.info('Generating AI response for:', raw_data);
     const input = JSON.stringify(raw_data);
-    functions.logger.info('Input data:', input);
+
     // prompt for generating temprorary feedback on the day's planning
-    const feedbackPrompt = `This object contains all the tasks that I have to do today: ${input}. But the day is not fully planned.
-    Assign a mental workload score between 1 and 5 after assessing all the details and also give feedback on how the day is being structured under 60 words. 
+    const feedbackPrompt = `This object contains all the tasks that I have to do so far today, 
+    how i felt doing my tasks today and my general work preferences: ${input}. 
+    But the day is not fully planned.
+    Assign a mental workload score between 1 and 5 after assessing all the details and also give feedback on how the day is being structured under 100 words. 
     Return an object of type {userId: string, date: Timestamp (DD-MM-YYYY), 
     mwl: number, feedback: string}. where mwl is the mental workload. Do not use backticks or format into markdown.`;
 
     // prompt for generating total mwl score
-    const totalMwlPrompt = `This object contains all the tasks that I have to do today: ${input}. 
+    const totalMwlPrompt = `This object contains all the tasks that I have to do today, 
+    how i felt doing my tasks today and my general work preferences: ${input}. 
     Assign a mental workload score between 1 and 5 after assessing all the details available. 
     Return an object of type {userId: string, date: Timestamp (DD-MM-YYYY), 
     mwl: number, feedback: string}. where mwl is the mental 
@@ -77,7 +80,7 @@ export const getAIMwlRating = async ({
 
     // construct the textPart
     const textPart = {
-      text: isTemporaryFeedback ? feedbackPrompt : totalMwlPrompt,
+      text: isTemporary ? feedbackPrompt : totalMwlPrompt,
     };
 
     const request = {
@@ -91,13 +94,9 @@ export const getAIMwlRating = async ({
     const fullTextResponse =
       aggregatedResponse.candidates?.[0].content.parts[0].text;
 
-    let correctedString = fullTextResponse
-      ?.replace(/(\w+):/g, '"$1":')
-      .replace(/\'/g, '"');
+    const correctedString = removeJsonCodeBlockTags(fullTextResponse || '');
 
-    correctedString = removeJsonCodeBlockTags(correctedString || '');
-
-    let returnResponse: AIMwlReturnType = JSON.parse(correctedString);
+    const returnResponse: AIMwlReturnType = JSON.parse(correctedString);
 
     return returnResponse;
   } catch (error) {
